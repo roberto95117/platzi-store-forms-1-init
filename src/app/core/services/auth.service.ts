178e9a3d4eeacc8010b67c9427080e30a32b93ef
RobertoXocop <roberto95117@gmail.com>
2022-12-28 23:52:11 -0600

    Update auth.service.ts

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,3 +1,5 @@
+import { tap } from 'rxjs/operators';
+import { TokenService } from './token.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
@@ -9,7 +11,8 @@ export class AuthService {
 
   constructor(
     private auth: AngularFireAuth,
-    private http: HttpClient
+    private http: HttpClient,
+    private token: TokenService
   ) { }
 
   createUser(email: string, password: string) {
@@ -32,6 +35,12 @@ export class AuthService {
     return this.http.post('https://platzi-store.herokuapp.com/auth',{
       email,
       password
-    });
+    })
+    .pipe(
+      tap((data : {token : string}) => {
+        const tokenVal = data.token;
+        this.token.saveToken(tokenVal);
+      })
+    );
   }
 }
