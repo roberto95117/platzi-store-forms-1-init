28a3752151fae60b4df7543f3f346ae7597b03af
RobertoXocop <roberto95117@gmail.com>
2022-12-29 17:44:09 -0600

    clase 10
    Google Analytics y Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
+import { NavigationEnd, Router } from '@angular/router';
 import { Component } from '@angular/core';
-
+import { filter} from 'rxjs/operators'
+declare var gtag;
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(
+    private roter: Router
+  ){
+    const navEndEvents$ =
+    this.roter.events
+    .pipe(
+      filter(event => event instanceof NavigationEnd)
+    );
+
+    navEndEvents$
+    .subscribe((event : NavigationEnd) => {
+      gtag('config', 'MEASUREMENT_ID'),{
+        page_path : event.urlAfterRedirects
+      }
+    });
+
+  }
+
 }
